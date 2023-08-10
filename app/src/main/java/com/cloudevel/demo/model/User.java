e5fbbbb3070dcff285b6647f0902ed1656536d17
Jose Luis Chiquete <josech@gmail.com>
2023-08-10 15:37:47 -0600

    230810c

--- app/src/main/java/com/cloudevel/demo/model/User.java
+++ app/src/main/java/com/cloudevel/demo/model/User.java
@@ -1,26 +1,21 @@
 package com.cloudevel.demo.model;
 
-\\ Clase que representa a un usuario
 
 public class User {
     private String email;
-    \\ Constructor vacío
 
     public User() {
     }
 
     public User(String email) {
-        \\ Constructor que recibe el correo electrónico del usuario
         this.email = email;
     }
 
     public String getEmail() {
-        \\ Método que devuelve el correo electrónico del usuario
         return email;
     }
 
     public void setEmail(String email) {
-        \\ Método que establece el correo electrónico del usuario
         this.email = email;
     }
 }
