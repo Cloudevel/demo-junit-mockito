0e54f1a3e6ed2bf1de90dd6c7add6045987a3d95
Jose Luis Chiquete <josech@gmail.com>
2023-08-10 15:33:44 -0600

    230810b

--- app/src/main/java/com/cloudevel/demo/App.java
+++ app/src/main/java/com/cloudevel/demo/App.java
@@ -1,3 +1,8 @@
+\\ Aplicación principal de la aplicación
+\\ Esta clase es la que se ejecuta al iniciar la aplicación
+\\ Aquí se crea una instancia de UserServiceImpl y se pasa como parámetro a UserController
+\\ UserController es el controlador de la aplicación, es el encargado de recibir las peticiones del usuario
+\\ y delegarlas a UserServiceImpl para que se realicen las operaciones necesarias
 package com.cloudevel.demo;
 
 import com.cloudevel.demo.controller.UserController;
@@ -5,6 +10,7 @@ import com.cloudevel.demo.service.UserService;
 import com.cloudevel.demo.service.UserServiceImpl;
 
 public class App {
+    \\ Método principal de la aplicación
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         UserController userController = new UserController(userService);
